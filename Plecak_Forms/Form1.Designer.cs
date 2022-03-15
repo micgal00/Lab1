6d5ffcf14a4f632b83f7deb3c03070b05c9c2c2b
micha <micha@DESKTOP-7DEVPFQ>
2022-03-15 11:10:42 +0100

    Dodaj pliki projektów.

---
+++ Plecak_Forms/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Plecak_Forms
+{
+    partial class Plecak
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Run = new System.Windows.Forms.Button();
+            this.tbSeed = new System.Windows.Forms.TextBox();
+            this.tbLiczba = new System.Windows.Forms.TextBox();
+            this.tbPojemnosc = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(44, 172);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 0;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = true;
+            // 
+            // tbSeed
+            // 
+            this.tbSeed.Location = new System.Drawing.Point(44, 27);
+            this.tbSeed.Name = "tbSeed";
+            this.tbSeed.Size = new System.Drawing.Size(100, 20);
+            this.tbSeed.TabIndex = 1;
+            // 
+            // tbLiczba
+            // 
+            this.tbLiczba.Location = new System.Drawing.Point(44, 74);
+            this.tbLiczba.Name = "tbLiczba";
+            this.tbLiczba.Size = new System.Drawing.Size(100, 20);
+            this.tbLiczba.TabIndex = 2;
+            // 
+            // tbPojemnosc
+            // 
+            this.tbPojemnosc.Location = new System.Drawing.Point(44, 125);
+            this.tbPojemnosc.Name = "tbPojemnosc";
+            this.tbPojemnosc.Size = new System.Drawing.Size(100, 20);
+            this.tbPojemnosc.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(19, 222);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(224, 252);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(263, 61);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(194, 412);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "seed";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "liczba przedmiotow";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "pojemność";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 487);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.tbPojemnosc);
+            this.Controls.Add(this.tbLiczba);
+            this.Controls.Add(this.tbSeed);
+            this.Controls.Add(this.Run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.TextBox tbSeed;
+        private System.Windows.Forms.TextBox tbLiczba;
+        private System.Windows.Forms.TextBox tbPojemnosc;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Plecak_Forms/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using Lab1;
+
+
+namespace Plecak_Forms
+{
+    public partial class Plecak : Form
+    {
+        public Plecak()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(tbLiczba.Text);
+            int seed = int.Parse(tbSeed.Text);
+            int size = int.Parse(tbPojemnosc.Text);
+
+            int suma = 0;
+
+            List<Przedmiot> przedmioty = new List<Przedmiot>();
+            List<Przedmiot> items = new List<Przedmiot>();
+
+            Plecak plecak = new Plecak(0, size, items);
+            Generator generator = new Generator(n, seed);
+            for (int i = 0; i < n; i++)
+            {
+                Przedmiot przedmiot = new Przedmiot(i + 1, generator.numbers[i], generator.numbers[n - i - 1]);
+                przedmioty.Add(przedmiot);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (przedmioty[i].Waga < plecak._Rozmiar - suma)
+                {
+                    plecak._Przedmioty.Add(przedmioty[i]);
+                    suma += przedmioty[i].Waga;
+                }
+            }
+        }
+    }
+  
+
+}
